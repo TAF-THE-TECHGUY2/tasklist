52c8da0ff7fe21ae08f1e29cf4e4961e50c5be0c
sizibaast <sizibaast@gmail.com>
2025-04-10 15:21:25 +0200

    edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -3,23 +3,24 @@
 @section('title', 'Add Task')
 
 @section('styles')
-<style>
-    .error-message{
-        color:red;
-        font-size: 0. 8rem;
-    }
-</style>
+    <style>
+        .error-message {
+            color: red;
+            font-size: 0. 8rem;
+        }
+    </style>
 @endsection
 
 @section('content')
     {{$errors}}
-    <form method="Post" action="{{route('tasks.store')}}">
+    <form method="Post" action="{{route('tasks.update' , ['id' => $task ->id]) }}">
         @csrf
+        @method('PUT')
         <div>
             <label for="title">
-              Title
+                Title
             </label>
-            <input text="text" name="title" id="title">
+            <input text="text" name="title" id="title" value="{{$task->title}}"/>
             @error('title')
                 <p class="error-message">{{$message}}</p>
             @enderror
@@ -27,7 +28,7 @@
 
         <div>
             <label for="description">Description </label>
-            <textarea name="description" id="description" rows="5"></textarea>
+            <textarea name="description" id="description" rows="5">value="{{$task->description}}</textarea>
             @error('description')
                 <p class="error-message ">{{$message}}</p>
             @enderror
@@ -35,13 +36,13 @@
 
         <div>
             <label for="long_description">Description </label>
-            <textarea name="long_description" id="long_description" rows="10"></textarea>
+            <textarea name="long_description" id="long_description" rows="10">value="{{$task->long_description}}</textarea>
             @error('long_description')
                 <p class="error-message">{{$message}}</p>
             @enderror
         </div>
         <div>
-            <button type="submit">Add Task</button>
+            <button type="submit">Edit task </button>
         </div>
     </form>
 @endsection
