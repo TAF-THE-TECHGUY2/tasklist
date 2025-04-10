a992947d69eaf41f7c01929c051f6db3fcf17abb
sizibaast <sizibaast@gmail.com>
2025-04-10 13:25:37 +0200

    form valdiation and storing in data base

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 
 <?php
-
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
-class Task
+/*class Task
 {
   public function __construct(
     public int $id,
@@ -56,7 +56,7 @@ $tasks = [
     '2023-03-04 12:00:00',
     '2023-03-04 12:00:00'
   ),
-];
+];*/
 
 Route::get('/', function ()  {
     return redirect() ->route('tasks.index');
@@ -64,7 +64,7 @@ Route::get('/', function ()  {
 
 Route::get('/task', function ()  {
     return view ('index' , [
-        'tasks'=>\App\Models\Task::latest()->get()
+        'tasks'=>Task::latest()->get()
     ]);
     })->name('tasks.index');
 
@@ -72,11 +72,23 @@ Route::view('/tasks/create', 'create')
     ->name('tasks.create');
 
 Route::get('/tasks/{id}', function ($id)  {
-return view('show', ['task' =>\App\Models\Task::findOrFail($id)]);
+return view('show', ['task' =>Task::findOrFail($id)]);
 })->name('tasks.show');
 
 Route::post('/tasks', function (Request $request)  {
-dd($request->all());
+
+    $data = $request -> validate([
+        'title'=> 'required|max:255',
+        'description'=> 'required',
+        'long_description'=> 'required',
+    ]);
+    $task = new Task;
+    $task -> title = $data['title'];
+    $task -> description = $data['description'];
+    $task ->long_description = $data['long_description'];
+    $task -> save();
+
+    return redirect()->route('tasks.show',['id' => $task->id]);
 })->name('tasks.store');
 
 
